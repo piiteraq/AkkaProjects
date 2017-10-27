b30bb9b7f9b3b73316fd055936661e465cd6b73b
Peter Haahr Christensen <peter.christensen@att.net>
2017-10-27 12:02:56 -0700

    Initial commit

---
+++ src/main/scala/ActorHierarchyExperiments.scala
@@ -0,0 +1,63 @@
+package net.scidane.akka.sample
+
+import akka.actor.{ Actor, Props, ActorSystem }
+import scala.io.StdIn
+
+class PrintMyActorRefActor extends Actor {
+  override def receive: Receive = {
+    case "printit" =>
+      val secondRef = context.actorOf(Props.empty, "second-actor")
+      println(s"Second: $secondRef")
+  }
+}
+
+class StartStopActor1 extends Actor {
+  override def preStart(): Unit = {
+    println("first started")
+    context.actorOf(Props[StartStopActor2], "second")
+  }
+  override def postStop(): Unit = println("first stopped")
+
+  override def receive: Receive = {
+    case "stop" => context.stop(self)
+  }
+}
+
+class StartStopActor2 extends Actor {
+  override def preStart(): Unit = println("second started")
+  override def postStop(): Unit = println("second stopped")
+
+  // Actor.emptyBehavior is a useful placeholder when we don't
+  // want to handle any messages in the actor.
+  override def receive: Receive = Actor.emptyBehavior
+}
+
+class SupervisingActor extends Actor {
+  val child = context.actorOf(Props[SupervisedActor], "supervised-actor")
+
+  override def receive: Receive = {
+    case "failChild" => child ! "fail"
+  }
+}
+
+class SupervisedActor extends Actor {
+  override def preStart(): Unit = println("supervised actor started")
+  override def postStop(): Unit = println("supervised actor stopped")
+
+  override def receive: Receive = {
+    case "fail" =>
+      println("supervised actor fails now")
+      throw new Exception("I failed!")
+  }
+}
+
+object ActorHierarchyExperiments extends App {
+  val system = ActorSystem("testSystem")
+
+  val supervisingActor = system.actorOf(Props[SupervisingActor], "supervising-actor")
+  supervisingActor ! "failChild"
+
+  println(">>> Press ENTER to exit <<<")
+  try StdIn.readLine()
+  finally system.terminate()
+}
