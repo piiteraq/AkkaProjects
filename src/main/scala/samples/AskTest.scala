6bd42386ad1fa975cc94750453b5f4cdb5b0d000
Peter Haahr Christensen <peter.christensen@att.net>
2018-03-10 13:05:34 -0800

    Changed example.

--- src/main/scala/samples/AskTest.scala
+++ src/main/scala/samples/AskTest.scala
@@ -3,9 +3,12 @@ package samples
 import akka.actor._
 import akka.pattern.ask
 import akka.util.Timeout
-import concurrent.{Await, ExecutionContext, Future}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 import concurrent.duration._
 import language.postfixOps
+import concurrent.{Await, ExecutionContext, Future}
 
 case object AskNameMessage
 
@@ -22,10 +25,20 @@ object AskTest extends App {
   val myActor = system.actorOf(Props[TestActor], name="myActor")
 
   try {
+
     implicit val timeout = Timeout(5 seconds)
-    val future = myActor ? AskNameMessage
-    val result = Await.result(future, timeout.duration).asInstanceOf[String]
-    println(result)
+    val fut: Future[Any] = myActor ? AskNameMessage
+
+    // Get results asynchronously
+    fut onComplete {
+      case Success(x) => println(s"Success: $x")
+      case Failure(ex) => println(s"Failed: $ex")
+    }
+    Thread.sleep(2000)
+
+    // Get results synchronously
+    //val result = Await.result(fut, timeout.duration).asInstanceOf[String]
+    //println(result)
   } finally {
     system.terminate()
   }
